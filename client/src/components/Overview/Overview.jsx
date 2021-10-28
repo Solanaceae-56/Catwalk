40af1cb0be8b315d005f690864398879c37e7168
Jinho Kum <Jinho.kum@gmail.com>
2021-10-28 12:04:12 -0700

    made server

--- client/src/components/Overview/Overview.jsx
+++ client/src/components/Overview/Overview.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
+import Star_Rating from './Star_Rating.jsx';
 
 class Overview extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      current_Product: {
+        "id": 40344,
+        "campus": "hr-rfp",
+        "name": "Camo Onesie",
+        "slogan": "Blend in to your crowd",
+        "description": "The So Fatigues will wake you up and fit you in. This high energy camo will have you blending in to even the wildest surroundings.",
+        "category": "Jackets",
+        "default_price": "140.00",
+        "created_at": "2021-08-13T14:38:44.509Z",
+        "updated_at": "2021-08-13T14:38:44.509Z"
+      },
+    };
   }
 
   render () {
     return (
       <div id='Overview'>
         <div id='product-info'>
-          <div id='star-rating'></div>
+          <div id='star-rating'><Star_Rating productId={this.state.current_Product.id}/></div>
           <div id='product-category'></div>
           <div id='product-title'></div>
           <div id='price'></div>
@@ -17,8 +31,13 @@ class Overview extends React.Component {
           <div id='social-media'></div>
         </div>
         <div id='style-selector'></div>
-        <div id='add-t0-cart'>
-          <div></div>
+        <div id='add-to-cart'>
+          <div id='size-selector'></div>
+          <div id='quantity-selector'></div>
+          <div id='add-to-cart'></div>
+        </div>
+        <div id='image-gallery'>
+          <div id='expanded-view'></div>
         </div>
       </div>
     )
--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  OverViewTOKEN: '',
-  RsTOKEN: '',
-  QATOKEN: '',
-  RelatedTOKEN: '',
-};
