63b6c7377fe24a04eb3ea656f75dd688e42aaa1d
Elton <eltonha4@gmail.com>
2021-10-28 12:11:37 -0700

    work on module

--- client/src/components/QuestionsAnswers.jsx
+++ client/src/components/QuestionsAnswers.jsx
@@ -1,18 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import QuestionsList from './QuestionsList.jsx';
+const axios = require('axios');
+const config = require('../../../config.js');
 
-class QuestionsAnswers extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      list: []
+function QuestionsAnswers () {
+  const [list, setList] = useState([]);
+  const options = {
+    url: 'url',
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': `token ${config.QATOKEN}`
     }
   }
+  useEffect( () => {
+    axios.get(options.url, options.headers).then( (response) => {
+      setList(response);
+    });
+  });
 
-  render() {
-    return(
-      <div>PR Pull</div>
-    );
-  }
+  return (
+    <div>
+      <div>Questions and Answers</div>
+      <QuestionsList questions={list} />
+    </div>
+  );
+
+
 }
+
+// class QuestionsAnswers extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       list: []
+//     }
+//   }
+
+//   render() {
+//     return(
+//       <div>PR Pull</div>
+//     );
+//   }
+// }
 
 export default QuestionsAnswers;
