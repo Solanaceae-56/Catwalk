8ea96205cf23793a3a52f954ea602be6d02f85f3
Elton <eltonha4@gmail.com>
2021-10-29 15:09:06 -0700

    Update react version

--- client/src/components/QuestionsAnswers/QuestionsAnswers.jsx
+++ client/src/components/QuestionsAnswers/QuestionsAnswers.jsx
@@ -3,14 +3,20 @@ import QuestionsList from './QuestionsList.jsx';
 const axios = require('axios');
 const config = require('../../../../config.js');
 
-function QuestionsAnswers () {
+function QuestionsAnswers (props) {
   const [list, setList] = useState([]);
-  useEffect( () => {
-    axios.get("http://localhost:3000/qa/questions", {params: {productId: props.productid}}).then((response) => {
-      console.log(response);
-      setList(response);
-    });
-  });
+  // useEffect( () => {
+  //   // axios.get("http://localhost:3000/qa/questions", {params: {productId: props.productid}}).then((response) => {
+  //   //   console.log(response);
+  //   //   setList(response);
+  //   // });
+  //   axios.get("http://localhost:3000/qa/questions", {params: {productId: 40344}}).then((response) => {
+  //     //console.log(response);
+  //     console.log(response.data.results);
+  //     setList(response.data.results);
+  //     console.log(list, 'yo');
+  //   });
+  // }, []);
 
   return (
     <div>
