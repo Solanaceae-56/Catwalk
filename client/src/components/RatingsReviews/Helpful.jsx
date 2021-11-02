3d8864903e6bc135dcc5862ed2e2a9f2fd658a56
mkr123 <mankairui@yahoo.com>
2021-11-02 11:02:58 -0700

    both helpful button disable after click

--- client/src/components/RatingsReviews/Helpful.jsx
+++ client/src/components/RatingsReviews/Helpful.jsx
@@ -7,20 +7,20 @@ export default function Helpful(props) {
     axios.put(serverUrl).then(()=>{
       setHelpCount(helpCount+1);
       document.getElementById(`helpful${props.review_id}`).disabled=true;
+      document.getElementById(`report${props.review_id}`).disabled=true;
     })
   }
   function handleReport(){
     let serverUrl = `/reviews/${props.review_id}/report`
     axios.put(serverUrl).then(()=>{
       document.getElementById(`report${props.review_id}`).disabled=true;
+      document.getElementById(`helpful${props.review_id}`).disabled=true;
     })
   }
   return (
     <div>
       <div>Helpful?<button id={`helpful${props.review_id}`} onClick={handleHelpful}>Yes</button><span>({helpCount})|</span>
       <button id={`report${props.review_id}`} onClick={handleReport}>Report</button></div>
-
-
     </div>
 
   )
