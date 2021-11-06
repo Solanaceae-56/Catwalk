94e28279abd2e8c6caec95bdd5c08d57d3cf01c5
Elton <eltonha4@gmail.com>
2021-11-05 19:52:39 -0700

    Refactor and cleanup code

--- client/src/components/QuestionsAnswers/Answer.jsx
+++ client/src/components/QuestionsAnswers/Answer.jsx
@@ -4,7 +4,7 @@ const axios = require('axios');
 
 function Answer(props) {
   //console.log(props.item);
-  const [helpful, setHelpful] = useState(props.item.helpfulness);
+  const [helpfulness, setHelpfulness] = useState(props.item.helpfulness);
 
   function click(e) {
     //debugger;
@@ -15,7 +15,7 @@ function Answer(props) {
     axios.put("http://localhost:3000/qa/questions/put", { path: putPath, answerId: aId }).then((response) => {
       //console.log(response);
       if (putPath === 'helpfulanswer') {
-        setHelpful(helpful + 1);
+        setHelpfulness(helpfulness + 1);
       }
     });
   };
@@ -28,23 +28,6 @@ function Answer(props) {
     answerer = <b>{props.item['answerer_name']}</b>
   }
 
-  // console.log(photomap, 'photomap');
-  // if (props.item.photos.length !== 0) {
-  //   debugger;
-  //   return (
-  //     <div className='answer' id={props.item.id}>
-  //       <div className='body'>A: {props.item.body}</div>
-  //       <div className='misc'>by {props.item['answerer_name']}, {moment.utc(props.item.date).format('MM/DD/YYYY')}</div>
-  //       <div className='photos'>
-  //         {photomap}
-  //       </div>
-  //       <div className='help'>Helpful?</div>
-  //       <div className='helpful' id="helpfulanswer" onClick={click}> Yes ({helpful})</div>
-  //       <div className='report' id='reportanswer' onClick={click}> Report? </div>
-  //     </div>
-  //   );
-  // }
-
   return (
     <div className='answer' id={props.item.id}>
       <div className='body'>A: {props.item.body}</div>
@@ -53,7 +36,7 @@ function Answer(props) {
         {photomap}
       </div>
       <div className='help'>Helpful?</div>
-      <div className='helpful' id="helpfulanswer" onClick={click}> Yes ({helpful})</div>
+      <div className='helpful' id="helpfulanswer" onClick={click}> Yes ({helpfulness})</div>
       <div className='report' id='reportanswer' onClick={click}> Report? </div>
     </div>
   )
