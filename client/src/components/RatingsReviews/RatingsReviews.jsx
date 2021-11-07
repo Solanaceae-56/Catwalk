cdf370c6e0765816efaa0bb81dd3e9392cca14a8
mkr123 <mankairui@yahoo.com>
2021-11-07 13:08:21 -0800

    add some css

--- client/src/components/RatingsReviews/RatingsReviews.jsx
+++ client/src/components/RatingsReviews/RatingsReviews.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, createContext } from 'react';
 import ReviewList from "./ReviewList.jsx";
 import RatingList from "./RatingList.jsx";
+import "./RatingsReviews.css";
 export const ReviewsContext = createContext();
 export default function RatingsReviews(props) {
   const [reviewState, setReviewState] = useState({
@@ -24,7 +25,7 @@ export default function RatingsReviews(props) {
   }, [props])
   return (
     <div className="review">
-      <h2>Ratings & Reviews</h2>
+      <h2 className="sectionTitle">Ratings & Reviews</h2>
       <ReviewList product_id={reviewState.product_id} averageRating={reviewState.rating} num_Of_Ratings={reviewState.num_Of_Ratings} />
 
     </div>
