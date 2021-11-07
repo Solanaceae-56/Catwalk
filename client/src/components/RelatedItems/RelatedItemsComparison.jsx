1a9f13ad231d3c530154e6f3c19fa46299389da7
Viola Feng <avidafeng@gmail.com>
2021-11-06 17:30:08 -0700

    updated code to remove hard coded product id

--- client/src/components/RelatedItems/RelatedItemsComparison.jsx
+++ client/src/components/RelatedItems/RelatedItemsComparison.jsx
@@ -5,18 +5,31 @@ import axios from 'axios';
 // import relatedItemsId from './relatedItemsId.js';
 
 const RelatedItemsComparison = (props) => {
-    //var defaultProduct_id = props.product_id;
-    var defaultProduct_id = 40349;
+    // var defaultProduct_id = props.product_id;
 
-    return (
-        <div className='related-products'>
-                <h5>RELATED PRODUCTS</h5>
-                <RelatedList   defaultProduct_id={defaultProduct_id} handleCardClick={props.handleCardClick}/>
-                <h5>YOUR OUTFIT</h5>
-                <OutfitList defaultProduct_id={defaultProduct_id}/>
-        </div>
+    const [defaultProduct_id, setDefaultId] = useState();
+    useEffect(() => {
+        setDefaultId(props.product_id);
+    },[props.product_id])
 
-    )
+
+    if (defaultProduct_id) {
+        return (
+            <div className='related-products'>
+                    <h5>RELATED PRODUCTS</h5>
+                    <RelatedList   defaultProduct_id={defaultProduct_id} handleCardClick={props.handleCardClick}/>
+                    <h5>YOUR OUTFIT</h5>
+                    <OutfitList defaultProduct_id={defaultProduct_id}/>
+            </div>
+
+        )
+
+    } else {
+        return (
+            <div>Loading</div>
+        )
+    }
+
 }
 
 export default RelatedItemsComparison;
