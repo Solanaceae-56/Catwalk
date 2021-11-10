ae693881f9c1d5fc94c9cfe6806f03c6ce562844
Viola Feng <avidafeng@gmail.com>
2021-11-10 09:31:38 -0800

    add a config example file

---
+++ config example.js
@@ -0,0 +1,7 @@
+//Make a copy of this config example file
+// and rename the copy to config.js.
+//Then provide an auth token in FILL ME IN in config.js file.
+
+odule.exports = {
+  token: 'FILL ME IN'
+};
