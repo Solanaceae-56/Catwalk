21b83d5ee5d9f6c641f3f5a6495b15cadb83c06e
Viola Feng <avidafeng@gmail.com>
2021-11-10 10:27:48 -0800

    small modification to config example file

--- example.config.js
+++ example.config.js
@@ -1,6 +1,3 @@
-//Make a copy of this config example file
-// and rename the copy to config.js.
-//Then provide an auth token in FILL ME IN in config.js file.
 module.exports = {
-  token: 'FILL ME IN'
+  token: 'fill'
 }
