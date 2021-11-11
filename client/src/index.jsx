48c19f5ba40b879cf07f99da52939d211f271e21
mkr123 <mankairui@yahoo.com>
2021-11-11 13:53:43 -0800

    pull from main

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -93,11 +93,7 @@ class App extends React.Component {
           {lightDark}
         </div>
         <div className='app-container' onClick={this.handleClickElement}>
-<<<<<<< HEAD
-           <Overview product_id={this.state.product_id} rating={this.state.rating} num_Of_Ratings={this.state.num_Of_Ratings} />
-=======
           <Overview product_id={this.state.product_id} rating={this.state.rating} num_Of_Ratings={this.state.num_Of_Ratings} darkmode={this.state.darkMode}/>
->>>>>>> ee54ed9b90de39469b399e880adada37872bb1a1
           <div id='questionsAnswers'><QuestionsAnswers productName={this.state.name} id={this.state.product_id} /></div>
           <div id='relatedItems'><RelatedItemsComparison handleCardClick={this.handleCardClick} product_id={this.state.product_id} /></div>
           <div id='ratingsReviews'><RatingsReviews product_id={this.state.product_id} rating={this.state.rating} num_Of_Ratings={this.state.num_Of_Ratings} /></div>
